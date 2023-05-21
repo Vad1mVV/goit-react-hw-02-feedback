0d32e48d72f31643074a932fb561dda2c3f9322b
Vad1mVV <vadimvtoryhin@gmail.com>
2023-05-21 06:48:24 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,35 +8,57 @@ export class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
   };
 
   
 
-  addGoodFeedback = (e) => {
+  addGoodFeedback = () => {
     this.setState({
-      good: this.state.good + 1
+      good: this.state.good + 1,
+      total: this.state.total + 1
     })
   }
 
-  addNeutralFeedback = (e) => {
+  addNeutralFeedback = () => {
     this.setState({
-      neutral: this.state.neutral + 1
+      neutral: this.state.neutral + 1,
+      total: this.state.total + 1
     })
   }
 
-  addBadFeedback = (e) => {
+  addBadFeedback = () => {
     this.setState({
-      bad: this.state.bad + 1
+      bad: this.state.bad + 1,
+      total: this.state.total + 1
     })
+  }
+
+  countPositiveFeedbackPercentage = ({good, total}) => {
+    if(total === 0) {
+      return 0
+    }
+    const percent = (good / total) * 100
+    return Math.round(percent);
   }
 
   render() {
     return (
       <>
         <Title title='Please leave feedback' />
-        <Feedback addGoodFeedback={this.addGoodFeedback} addNeutralFeedback={this.addNeutralFeedback} addBadFeedback={this.addBadFeedback} />
+        <Feedback 
+          addGoodFeedback={this.addGoodFeedback} 
+          addNeutralFeedback={this.addNeutralFeedback} 
+          addBadFeedback={this.addBadFeedback}
+        />
         <Title title='Statistic' />
-        <Statistic good={this.state.good}  neutral={this.state.neutral}  bad={this.state.bad}/>
+        <Statistic 
+          good={this.state.good}  
+          neutral={this.state.neutral}  
+          bad={this.state.bad} 
+          total={this.state.total} 
+          positivePercentage={this.countPositiveFeedbackPercentage(this.state)}
+        />
       </>
     );
   }
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,15 +1,26 @@
 import PropTypes from 'prop-types';
+import { Notification } from '../Notification';
 
-export const Statistic = ({good, neutral, bad}) => {
-    return <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-    </ul>
+export const Statistic = ({good, neutral, bad, total, positivePercentage}) => {
+    return <div>
+        {total === 0 ? (
+        <Notification message='There is no feedback' />
+        ) : (
+        <ul>
+            <li>Good: {good}</li>
+            <li>Neutral: {neutral}</li>
+            <li>Bad: {bad}</li>
+            <li>Total: {total}</li>
+            <li>Positive feedbak: {positivePercentage}%</li>
+        </ul>
+        )}
+    </div>
 }
 
 Statistic.propTypes = {
     good: PropTypes.number,
     neutral: PropTypes.number,
     bad: PropTypes.number,
+    total: PropTypes.number,
+    positivePercentage: PropTypes.number,
 }
