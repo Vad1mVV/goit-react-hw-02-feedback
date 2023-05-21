7b8979159a772ff5fa6fe26facadbb639432d3cc
Vad1mVV <vadimvtoryhin@gmail.com>
2023-05-21 05:37:00 +0300

    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { Title } from "./Title";
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <>
+        <Title title='Please leave feedback' />
+        <Title title='Statistic' />
+      </>
+    );
+  }
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export const Feedback = () => {
+    return <>
+        <button type='button'>Good</button>
+        <button type='button'>Neutral</button>
+        <button type='button'>Bad</button>
+    </>
+}
